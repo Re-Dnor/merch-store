1045b608be26c0122aa1c1511b1385296e737d24
Mokshin Alexandr <awmokshin@gmail.com>
2023-03-15 16:16:58 +0300

    use hashrouter

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,12 +1,12 @@
 import { ReactElement } from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store } from "app/store";
 
 export const withProviders = (app: () => ReactElement) => () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>{app()}</Provider>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
